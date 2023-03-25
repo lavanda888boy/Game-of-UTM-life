2dcc72ad872ffcdc892c1035ae345e2a4fc06aab
alya1007 <alexandra.konjevic@isa.utm.md>
2023-03-25 21:30:29 +0200

    population control implementation

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,6 +7,8 @@ public class Game : MonoBehaviour
 
     private static int SCREEN_WIDTH = 86;
     private static int SCREEN_HEIGHT = 48;
+    public float speed = 0.05f;
+    private float timer = 0.0f;
 
     Cell[,] grid = new Cell[SCREEN_WIDTH, SCREEN_HEIGHT];
 
@@ -17,7 +19,16 @@ public class Game : MonoBehaviour
 
     void Update()
     {
-
+        if (timer >= speed)
+        {
+            timer = 0.0f;
+            CountNeighbors();
+            PopulationControl();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
     }
 
     void PlaceCells()
@@ -37,13 +48,128 @@ public class Game : MonoBehaviour
     bool RandomAliveCell()
     {
         int rand = UnityEngine.Random.Range(0, 100);
-        if (rand > 85)
+        if (rand > 65)
         {
             return true;
         }
         else
         {
             return false;
+        }
+    }
+
+    void CountNeighbors()
+    {
+        for (int y = 0; y < SCREEN_HEIGHT; y++)
+        {
+            for (int x = 0; x < SCREEN_WIDTH; x++)
+            {
+                int numNeighbors = 0;
+
+                // North
+                if (y + 1 < SCREEN_HEIGHT)
+                {
+                    if (grid[x, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // North East
+                if (y + 1 < SCREEN_HEIGHT && x + 1 < SCREEN_WIDTH)
+                {
+                    if (grid[x + 1, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // East
+                if (x + 1 < SCREEN_WIDTH)
+                {
+                    if (grid[x + 1, y].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // South East
+                if (y - 1 >= 0 && x + 1 < SCREEN_WIDTH)
+                {
+                    if (grid[x + 1, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // South
+                if (y - 1 >= 0)
+                {
+                    if (grid[x, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // South West
+                if (y - 1 >= 0 && x - 1 >= 0)
+                {
+                    if (grid[x - 1, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // West
+                if (x - 1 >= 0)
+                {
+                    if (grid[x - 1, y].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                // North West
+                if (y + 1 < SCREEN_HEIGHT && x - 1 >= 0)
+                {
+                    if (grid[x - 1, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                grid[x, y].numNeighbors = numNeighbors;
+            }
+        }
+    }
+
+    void PopulationControl()
+    {
+        // Rules
+        // 1. Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
+        // 2. Any live cell with two or three live neighbors lives on to the next generation.
+        // 3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+        // 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+        for (int y = 0; y < SCREEN_HEIGHT; y++)
+        {
+            for (int x = 0; x < SCREEN_WIDTH; x++)
+            {
+                if (grid[x, y].isAlive)
+                {
+                    if (grid[x, y].numNeighbors != 2 && grid[x, y].numNeighbors != 3)
+                    {
+                        grid[x, y].SetAlive(false);
+                    }
+                }
+                else
+                {
+                    if (grid[x, y].numNeighbors == 3)
+                    {
+                        grid[x, y].SetAlive(true);
+                    }
+                }
+            }
         }
     }
 }
